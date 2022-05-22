c44463a85ea836ffd14e4142ce089652e63c6800
michael <michael.matve@gmail.com>
2022-05-22 22:08:37 +0300

    finish tide valgrind

--- sources/OrgChart.cpp
+++ sources/OrgChart.cpp
@@ -5,15 +5,103 @@ namespace ariel{
 //////////////////////////////////////////////////////////////////
 ///////////////OrgChart class ////////////////////////////////////
 //////////////////////////////////////////////////////////////////
+    //constractors
 
     OrgChart::OrgChart()
     {
         this->root = nullptr;//start root to null ptr
     }
 
+    OrgChart::OrgChart(OrgChart& temp_tree)
+    {
+        if(temp_tree.root == nullptr)
+        {
+            this->root = nullptr;
+        }
+        else
+        {
+            this->root = new ONode(temp_tree.root->data);
+            *this = temp_tree;
+        }
+    }
+    //distractor
+    OrgChart::~OrgChart()
+    {
+        distractor_helper(this->root);
+    }
+
+    void OrgChart::distractor_helper(ONode* curr)
+    {
+        if(curr != nullptr)
+        {
+            if(curr->brother != nullptr)
+            {
+                distractor_helper(curr->brother);
+            }
+            if(curr->child != nullptr)
+            {
+                distractor_helper(curr->child);
+            }
+            delete(curr);
+        }
+    }
+    
+    // OrgChart::OrgChart(OrgChart&& temp_tree) noexcept
+    // {
+    //     *this = temp_tree;
+    // }
+
+    //operators for dip copy
+    OrgChart& OrgChart::operator= (const OrgChart& temp_tree)
+    {
+        if(this == &temp_tree)
+        {
+            return *this;
+        }
+        if(temp_tree.root == nullptr)
+        {
+            this->root = nullptr;
+        }
+        else
+        {
+        this->root = new ONode(temp_tree.root->data);
+        this->copy_helper(this->root,temp_tree.root);
+        }
+        return *this;
+    }
+    void OrgChart::copy_helper (ONode* new_node, ONode* old_node)
+    {
+        if(old_node == nullptr)
+        {
+            return;
+        }
+        if(old_node->child != nullptr)
+        {
+            new_node->child = new ONode(old_node->data);
+            // new_node->child->father = old_node;
+            ONode* new_children = new_node->child;
+            ONode* old_children = old_node->child;
+            while (old_children!=nullptr)
+            {
+                new_children->father = old_node;
+                copy_helper(new_children,old_children);
+                 if(old_children->brother != nullptr)
+                 {
+                    new_children->brother =  new ONode(old_children->brother->data);
+                 }
+                new_children = new_children->brother;
+                old_children = old_children->brother;
+            }
+        } 
+    }
+    
     //change root
     OrgChart& OrgChart::add_root(std::string data)
     {
+        if(data.empty() || data == "\n"|| data == "\t")
+        {
+            throw std::invalid_argument("cant get that string");
+        }
         //if the root is null make new node to the root
         if(this->root == nullptr)
         {
@@ -25,8 +113,12 @@ namespace ariel{
     }
 
     //add children
-    OrgChart& OrgChart::add_sub(std::string father, std::string son)
+    OrgChart& OrgChart::add_sub(const std::string& father, std::string son)
     {
+        if(son.empty() || son == "\n"|| son == "\t")
+        {
+            throw std::invalid_argument("cant get that string");
+        }
         //find the father
         ONode* pointer_father = this->find(father);
         //if the pather not found throw exp
@@ -57,7 +149,7 @@ namespace ariel{
         return *this;
     }
 
-    OrgChart::ONode* OrgChart::find(std::string data)
+    OrgChart::ONode* OrgChart::find(const std::string& data)
     {
         //run on the level order operator and check if the data is the same 
         for (auto it = this->begin_level_order(); it != this->end_level_order(); ++it)
@@ -118,60 +210,140 @@ namespace ariel{
         return My_output;
     }
     //iterators start and end
-    OrgChart::Iterator OrgChart::begin_level_order()
+
+    OrgChart::Iterator_level_order OrgChart::begin_level_order()
     {
         //return the start of the itrshion and set a vector with the true order
-        return Iterator(this->root).full_tor();
+        if(this->root == nullptr)
+        {
+            throw std::invalid_argument("null ptr");
+        }
+        return Iterator_level_order(this->root);
     }
 
-    OrgChart::Iterator OrgChart::end_level_order()
+    OrgChart::Iterator_level_order OrgChart::end_level_order()
     {
+        if(this->root == nullptr)
+        {
+            throw std::invalid_argument("null ptr");
+        }
         return nullptr;
     }
     
-    OrgChart::Iterator OrgChart::begin_reverse_order()
+    OrgChart::Iterator_reverse_order OrgChart::begin_reverse_order()
     {
+        if(this->root == nullptr)
+        {
+            throw std::invalid_argument("null ptr");
+        }
         //return the start of the itrshion and set a vector with the true order
-        return Iterator(this->root).full_stack();
+        return Iterator_reverse_order(this->root).full_stack();
     }
 
-    OrgChart::Iterator OrgChart::reverse_order()
+    OrgChart::Iterator_reverse_order OrgChart::reverse_order()
     {
+        if(this->root == nullptr)
+        {
+            throw std::invalid_argument("null ptr");
+        }
         return nullptr;
     }
 
-    OrgChart::Iterator OrgChart::begin_preorder()
+    OrgChart::Iterator_preorder OrgChart::begin_preorder()
     {
+        if(this->root == nullptr)
+        {
+            throw std::invalid_argument("null ptr");
+        }
         //return the start of the itrshion and set a vector with the true order
-        return Iterator(this->root).full_vec();
+        return Iterator_preorder(this->root);
     }
 
-    OrgChart::Iterator OrgChart::end_preorder()
+    OrgChart::Iterator_preorder OrgChart::end_preorder()
     {
+        if(this->root == nullptr)
+        {
+            throw std::invalid_argument("null ptr");
+        }
         return nullptr;
     }
  
-    OrgChart::Iterator OrgChart::begin()
+    OrgChart::Iterator_level_order OrgChart::begin()
     {
         return this->begin_level_order();
     }
 
-    OrgChart::Iterator OrgChart::end()
+    OrgChart::Iterator_level_order OrgChart::end()
     {
         return this->end_level_order();
     }
  
-    std::string& OrgChart::Iterator::operator*()
+ ////////////////level_order/////////////////////////////////
+    std::string& OrgChart::Iterator_level_order::operator*()
     {
         return this->ptr->data;
     }
 
-    std::string* OrgChart::Iterator::operator->()
+    std::string* OrgChart::Iterator_level_order::operator->()
     {
         return &(this->ptr->data);
     }
     
-    OrgChart::Iterator& OrgChart::Iterator::operator++()
+    OrgChart::Iterator_level_order& OrgChart::Iterator_level_order::operator++()
+    {
+            ONode* child = this->ptr->child;
+            while (child!=nullptr)
+            {
+                this->ptr_vec.push_back(child);
+                child = child->brother;
+            }
+        //if the vector is empty return nullptr get to end
+        if(this->ptr_vec.empty())
+        {
+            this->ptr = nullptr;
+        }
+        //else set this->ptr to the ptr_vec in the place
+        else
+        {
+            //change the pointer to the firest in the vector and remove it from the vector
+            this->ptr = this->ptr_vec[0];
+            this->ptr_vec.erase(this->ptr_vec.begin());
+            //add all the node child pointers to the vector
+        }
+        return *this;
+    }
+    
+    OrgChart::Iterator_level_order OrgChart::Iterator_level_order::operator++(int)
+    {
+        Iterator_level_order temp = *this;
+        ++(*this);
+        return temp;
+    }
+    
+    bool OrgChart::Iterator_level_order::operator!=(const Iterator_level_order &other) const
+    {
+        //check the pointers are not the same
+        return this->ptr!=other.ptr;
+    }
+     //return the iterators pointer to the node
+    OrgChart::ONode* OrgChart::Iterator_level_order::get_ptr()
+    {
+        return this->ptr;
+    }
+
+
+    ////////////////Iterator_reverse_order/////////////////////////////////
+    std::string& OrgChart::Iterator_reverse_order::operator*()
+    {
+        return this->ptr->data;
+    }
+
+    std::string* OrgChart::Iterator_reverse_order::operator->()
+    {
+        return &(this->ptr->data);
+    }
+    
+    OrgChart::Iterator_reverse_order& OrgChart::Iterator_reverse_order::operator++()
     {
         //change the curr place
         ++this->place;
@@ -188,49 +360,20 @@ namespace ariel{
         return *this;
     }
     
-    OrgChart::Iterator OrgChart::Iterator::operator++(int)
+    OrgChart::Iterator_reverse_order OrgChart::Iterator_reverse_order::operator++(int)
     {
-        Iterator temp = *this;
+        Iterator_reverse_order temp = *this;
         ++(*this);
         return temp;
     }
     
-    bool OrgChart::Iterator::operator!=(const Iterator &other) const
+    bool OrgChart::Iterator_reverse_order::operator!=(const Iterator_reverse_order &other) const
     {
         //check the pointers are not the same
-        if(this->ptr!=other.ptr)
-        {
-            return true;
-        }
-        return false;
+        return this->ptr!=other.ptr;
     }
-
-    OrgChart::Iterator& OrgChart::Iterator::full_tor()
-    {
-        //check the tree is not null
-        if(this->ptr == nullptr)
-        {
-            return *this;
-        }
-    //add the root to the vector
-       this->ptr_vec.push_back(this->ptr);
-       ONode* curr_node  = this->ptr;
-       ulong tor_place = 0;
-        //run on all the nodes in the vector and add there cildred to the vector
-       while(tor_place < this->ptr_vec.size())
-       {   
-           curr_node = this->ptr_vec[tor_place]->child;
-           while(curr_node != nullptr)
-           {
-               this->ptr_vec.push_back(curr_node);
-               curr_node = curr_node->brother;
-           }
-           ++tor_place;
-       }
-       return *this;
-    }
-
-    OrgChart::Iterator& OrgChart::Iterator::full_stack()
+    
+    OrgChart::Iterator_reverse_order& OrgChart::Iterator_reverse_order::full_stack()
     {
         //check the tree is not null
         if(this->ptr == nullptr)
@@ -239,12 +382,11 @@ namespace ariel{
         }
         //add the root at the begining
        this->ptr_vec.insert(this->ptr_vec.begin(),this->ptr);
-       ONode* curr_node  = this->ptr;
        ulong tor_place = 0;
         //run on all the nodes in the vector from the end and add all the children of them in the begining
         while(tor_place < this->ptr_vec.size())
         {   
-           curr_node = this->ptr_vec[this->ptr_vec.size()- tor_place -1]->child;
+           ONode* curr_node = this->ptr_vec[this->ptr_vec.size()- tor_place -1]->child;
            //add the children in there true order
            int index =0;
            while(curr_node != nullptr)
@@ -258,43 +400,63 @@ namespace ariel{
        this->ptr = this->ptr_vec[0];
        return *this;
     }
-
-    OrgChart::Iterator& OrgChart::Iterator::full_vec()
-    {
-        ONode* curr_node = this->ptr;
-        //add the root to vector
-        this->ptr_vec.push_back(curr_node);
-        //run antil we get to nullptr
-        while(curr_node!= nullptr)
-        {
-            //go to the last child of child and add it to the vector
-            while(curr_node->child != nullptr)
-            {
-                curr_node= curr_node->child;
-                this->ptr_vec.push_back(curr_node);
-            }
-            //go back in the levels antil get to one that has brother
-            while(curr_node->brother == nullptr)
-            {
-                curr_node = curr_node->father;
-                //if got to null finished
-                if(curr_node == nullptr)
-                {
-                    break;
-                }
-            }
-            //if found node with brother go to the brother add him to the vector and check his children
-            if(curr_node != nullptr)
-            {
-                curr_node= curr_node->brother;
-                this->ptr_vec.push_back(curr_node);
-            }
-        }
-        return *this;
-    }
-    //return the iterators pointer to the node
-    OrgChart::ONode* OrgChart::Iterator::get_ptr()
+     //return the iterators pointer to the node
+    OrgChart::ONode* OrgChart::Iterator_reverse_order::get_ptr()
     {
         return this->ptr;
     }
+ 
+ ////////////////Iterator_preorder/////////////////////////////////
+    std::string& OrgChart::Iterator_preorder::operator*()
+    {
+        return this->ptr->data;
+    }
+
+    std::string* OrgChart::Iterator_preorder::operator->()
+    {
+        return &(this->ptr->data);
+    }
+    
+    OrgChart::Iterator_preorder& OrgChart::Iterator_preorder::operator++()
+    {
+        //check if there is child
+        if(this->ptr->child!= nullptr)
+        {
+           this->ptr = this->ptr->child;
+           return *this;
+        }
+        //if there is brother return him else return null
+        while(this->ptr->brother == nullptr)
+        {
+            //when no father return nullptr
+            if(this->ptr->father == nullptr)
+            {
+                this->ptr = nullptr;
+                return *this;
+            }
+            this->ptr = this->ptr->father;
+        }
+        this->ptr = this->ptr->brother;
+        return *this;
+    }
+    
+    OrgChart::Iterator_preorder OrgChart::Iterator_preorder::operator++(int)
+    {
+        Iterator_preorder temp = *this;
+        ++(*this);
+        return temp;
+    }
+    
+    bool OrgChart::Iterator_preorder::operator!=(const Iterator_preorder &other) const
+    {
+        //check the pointers are not the same
+        return this->ptr!=other.ptr;
+    }
+  
+     //return the iterators pointer to the node
+    OrgChart::ONode* OrgChart::Iterator_preorder::get_ptr()
+    {
+        return this->ptr;
+    }
+
 }
--- sources/OrgChart.hpp
+++ sources/OrgChart.hpp
@@ -29,64 +29,129 @@ namespace ariel{
         //constarctor
         OrgChart();
 
-        //change root
+        //copy constractor
+        OrgChart(OrgChart& temp_tree);
+
+        ~OrgChart();
+        //recursive function to remove all nodes
+        void distractor_helper(ONode* curr);
+        //move constractor
+        // OrgChart(OrgChart&& temp_tree) noexcept;
+
+        //dipp copy
+        OrgChart& operator= (const OrgChart& temp_tree);
+        
+        //copy helper 
+        void copy_helper (ONode* new_node, ONode* old_node);
+        
+        // move 
+        // OrgChart& operator= (const OrgChart&& temp_tree) noexcept;
+
         OrgChart& add_root(std::string data);
 
         //add children
-        OrgChart& add_sub(std::string father, std::string son);
+        OrgChart& add_sub(const std::string& father, std::string son);
 
         //find node with the same data
-        ONode* find(std::string data);
+        ONode* find(const std::string& data);
         //print
         friend std::ostream& operator<<(std::ostream& My_output,const OrgChart& tree);
-        //iterator
-        class Iterator
+    ////////////////// level_order ////////////////////
+    class Iterator_level_order
         {
             ONode * ptr;
             std::vector<ONode*> ptr_vec;
-            ulong place;
         
             public:
-                Iterator(ONode* new_ptr)
+                Iterator_level_order(ONode* new_ptr)
                 {
                     this->ptr = new_ptr;
-                    this->place = 0;
                 }
                 std::string& operator*();
 
-                Iterator& operator++();
+                Iterator_level_order& operator++();
                 
-                Iterator operator++(int);
+                Iterator_level_order operator++(int);
                 
-                bool operator!=(const Iterator &other) const ;
-                //function that help to full the vector
-                Iterator& full_tor();
-                
-                Iterator& full_stack();
-
-                Iterator& full_vec();
+                bool operator!=(const Iterator_level_order &other) const ;
 
                 std::string* operator->();
 
                 ONode* get_ptr();
 
     };
+    
+    //////////////////reverse_order////////////////////
+    class Iterator_reverse_order
+        {
+            ONode * ptr;
+            std::vector<ONode*> ptr_vec;
+            ulong place;
+        
+            public:
+                Iterator_reverse_order(ONode* new_ptr)
+                {
+                    this->ptr = new_ptr;
+                    this->place = 0;
+                }
+                std::string& operator*();
 
-    Iterator begin();
+                Iterator_reverse_order& operator++();
+                
+                Iterator_reverse_order operator++(int);
+                
+                bool operator!=(const Iterator_reverse_order &other) const ;
+                
+                Iterator_reverse_order& full_stack();
 
-    Iterator end();
+                std::string* operator->();
+
+                ONode* get_ptr();
+
+    };
+    
+    /////////////////preorder//////////////////////////
+    class Iterator_preorder
+        {
+            ONode * ptr;
+            public:
+                Iterator_preorder(ONode* new_ptr)
+                {
+                    this->ptr = new_ptr;
+                }
+                std::string& operator*();
+
+                Iterator_preorder& operator++();
+                
+                Iterator_preorder operator++(int);
+                
+                bool operator!=(const Iterator_preorder &other) const ;
+                //function that help to full the vector
+
+                std::string* operator->();
+
+                ONode* get_ptr();
+
+    };
+    
+    Iterator_level_order begin();
+
+    Iterator_level_order end();
     // level order
-    Iterator begin_level_order();
+    Iterator_level_order begin_level_order();
 
-    Iterator end_level_order();
+    Iterator_level_order end_level_order();
+
     //revers order
-    Iterator begin_reverse_order();
+    Iterator_reverse_order begin_reverse_order();
 
-    Iterator reverse_order();
+    Iterator_reverse_order reverse_order();
+    
     //preorder order
-    Iterator begin_preorder();
+    Iterator_preorder begin_preorder();
 
-    Iterator end_preorder();
+    Iterator_preorder end_preorder();
+
     };
 }
 
